9e037f0ff33acab467db7685a6638f4c1e56f849
Roy Schrauwen <roy@royschrauwen.nl>
2022-01-07 18:03:24 +0100

    Made helper function to var_dump easier

--- app/init.php
+++ app/init.php
@@ -2,3 +2,14 @@
 
 require_once ('core/App.php');
 require_once ('controllers/Controller.php');
+
+// PRE VARDUMP -- HELPER FUNCTIE DIE DE VARDUPS PRE TAGS GEEFT
+function pvd($stringToDisplay)
+{
+    echo "<div style=\"background-color: lightgray;\">";
+    echo "<h3 style=\"margin-bottom: 0\">" . $stringToDisplay . "</h3>";
+    echo "<pre>";
+    var_dump($stringToDisplay);
+    echo "</pre>";
+    echo "</div>";
+}
