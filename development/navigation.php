07723b3acad44baa5ea82c8e8297db1b57992e69
Roy Schrauwen <roy@royschrauwen.nl>
2022-01-25 18:34:23 +0100

    Seperate include file for navigation

---
+++ development/navigation.php
@@ -0,0 +1,9 @@
+<nav>     
+            <h4>Navigatiemenu</h4>
+            <ul>
+                <li>Requirements-weergave</li>
+                <li>Requirement toevoegen</li>
+                <li>Project-instellingen</li>
+                <li>Gebruikers-instellingen</li>
+            </ul>
+        </nav>
