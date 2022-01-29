5b5b9f476dd7236068a5028ddd556be63c194102
Roy Schrauwen <roy@royschrauwen.nl>
2022-01-29 10:09:36 +0100

    Used objects instead of array

--- development/index.php
+++ development/index.php
@@ -24,7 +24,7 @@ include ("navigation.php");
                     for ($i = 0; $i < count($project->getCategories()); $i++) {
                         echo "<div class=moscow-card>";
                         echo "<h3>" . $project->getCategories()[$i]->getName() . "</h3>";
-                        $project->showAllRequirementsOfCategory($i);
+                        $project->showAllRequirementsOfCategory($project->getCategories()[$i]);
                         echo "</div>";
                     }
                     ?>
