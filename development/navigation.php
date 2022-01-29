31d48e73ad51d7d732aa255582c10de3af22bda9
Roy Schrauwen <roy@royschrauwen.nl>
2022-01-29 19:32:14 +0100

    Made nav sticky

--- development/navigation.php
+++ development/navigation.php
@@ -2,7 +2,7 @@
             <ul>
                 <li><a href="index.php?view=furps">Requirements<br>FURPS weergave</a></li>
                 <li><a href="index.php?view=moscow">Requirements<br>MoSCoW weergave</a></li>
-                <li>Requirement toevoegen</li>
+                <li><a href="index.php?view=newreq">Requirement toevoegen</a></li>
                 <li>Project-instellingen</li>
                 <li>Gebruikers-instellingen</li>
             </ul>
